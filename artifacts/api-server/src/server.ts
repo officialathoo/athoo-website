c253961f2e2e98749706b3ff23b3e617e4b8451b
Athoo <official.athoo@gmail.com>
2026-05-27 22:48:35 +0500

    Fix api-server NodeNext imports

--- artifacts/api-server/src/server.ts
+++ artifacts/api-server/src/server.ts
@@ -1,6 +1,6 @@
-import app from "./app";
-import { logger } from "./lib/logger";
-import { ensureSchema } from "./lib/dbInit";
+import app from "./app.js";
+import { logger } from "./lib/logger.js";
+import { ensureSchema } from "./lib/dbInit.js";
 
 const rawPort = process.env["PORT"] || "8080";
 
